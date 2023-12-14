e9376c85775446dab8f35c03c4c041997fe434b3
DefinitelyNotAnAssassin <dharklike@gmail.com>
2023-12-15 00:32:37 +0800

    Commented Interface

--- src/main/java/com/mycompany/studentkiosksystem/AccountInterface.java
+++ src/main/java/com/mycompany/studentkiosksystem/AccountInterface.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public interface AccountInterface 
 {
-    public void createAccount() throws SQLException;
+    public void createAccount() throws SQLException; // For creating an account
     public abstract boolean logIn(String SN, String pass)throws SQLException; 
-    
+    //for user authentication
 }
